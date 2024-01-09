c7c9656a7c098f09ba5ae416497c43735a9d4950
Tonmoy Roy <81863666+tonmoy-dev@users.noreply.github.com>
2024-01-09 04:55:08 +0000

    js data types concepts added

---
+++ 0_basics/02_data-types.js
@@ -0,0 +1,41 @@
+// Data types and ECMA standards
+
+"use strict"; // treat all JS code as newer version
+
+// alert() // we're using nodejs, not brower
+
+/* write readable codes - use semicolons after any statement 
+# code readability should be high
+*/
+
+/* Read documnentaions in JavaScript
+For ECMAScript standards - https://tc39.es/
+MDN - https://developer.mozilla.org/en-US/docs/Web/JavaScript
+*/
+// The ECMAScript language types are Undefined, Null, Boolean, String, Symbol, Number, BigInt, and Object. 
+
+
+let name = "rx"  // string
+let age = 20  // number
+let loggedIn = false  // boolean
+let state = null  // null type
+
+/*
+Premitive data types
+// number 
+// bigint
+// string => ""
+// boolean => true/ false
+// null => a standalone value too; but it is a representation of empty value
+// undefined => declared variable but not assign any value
+// symbol => means unique values
+
+*/
+
+// Object data type - a non-primitive data type that consists of unordered key-value pairs. 
+
+// use 'typeof' keyword to check the datatype
+console.log(typeof "devs")
+console.log(typeof null)  // object
+console.log(typeof undefined)  // undefined
+
